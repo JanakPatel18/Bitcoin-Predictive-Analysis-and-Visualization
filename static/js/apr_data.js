ee3c643abbcb22ea494cd41797c5844a5685460f
Janak Patel <91707734+JanakPatel18@users.noreply.github.com>
2022-03-20 22:58:40 -0400

    Add files via upload

---
+++ static/js/apr_data.js
@@ -0,0 +1,310 @@
+ var data = [
+  {
+    Date: "03/01/21",
+     Weighted_Price : " $47,640.76 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/02/21",
+     Weighted_Price : " $48,617.29 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/03/21",
+     Weighted_Price : " $50,426.99 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/04/21",
+     Weighted_Price : " $49,398.25 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/05/21",
+     Weighted_Price : " $47,717.64 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/06/21",
+     Weighted_Price : " $48,372.18 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/07/21",
+     Weighted_Price : " $50,214.47 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/08/21",
+     Weighted_Price : " $50,797.91 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/09/21",
+     Weighted_Price : " $53,980.85 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/10/21",
+     Weighted_Price : " $55,373.18 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/11/21",
+     Weighted_Price : " $56,272.18 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/12/21",
+     Weighted_Price : " $56,826.56 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/13/21",
+     Weighted_Price : " $58,793.70 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/14/21",
+     Weighted_Price : " $60,470.10 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/15/21",
+     Weighted_Price : " $57,673.99 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/16/21",
+     Weighted_Price : " $55,333.38 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/17/21",
+     Weighted_Price : " $56,065.98 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/18/21",
+     Weighted_Price : " $58,509.43 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/19/21",
+     Weighted_Price : " $58,263.34 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/20/21",
+     Weighted_Price : " $58,775.05 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/21/21",
+     Weighted_Price : " $57,347.44 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/22/21",
+     Weighted_Price : " $56,910.99 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/23/21",
+     Weighted_Price : " $54,701.37 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/24/21",
+     Weighted_Price : " $55,215.24 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/25/21",
+     Weighted_Price : " $52,161.53 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/26/21",
+     Weighted_Price : " $53,114.81 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/27/21",
+     Weighted_Price : " $55,133.56 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/28/21",
+     Weighted_Price : " $55,851.17 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/29/21",
+     Weighted_Price : " $56,831.01 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/30/21",
+     Weighted_Price : " $58,300.80 ",
+    Forecast: ""
+  },
+  {
+    Date: "03/31/21",
+     Weighted_Price : " $58,667.40 ",
+    Forecast: ""
+  },
+  {
+    Date: "04/01/21",
+     Weighted_Price : "",
+    Forecast: "$61,519.78"
+  },
+  {
+    Date: "04/02/21",
+     Weighted_Price : "",
+    Forecast: "$59,958.74"
+  },
+  {
+    Date: "04/03/21",
+     Weighted_Price : "",
+    Forecast: "$57,997.02"
+  },
+  {
+    Date: "04/04/21",
+     Weighted_Price : "",
+    Forecast: "$59,311.69"
+  },
+  {
+    Date: "04/05/21",
+     Weighted_Price : "",
+    Forecast: "$59,668.55"
+  },
+  {
+    Date: "04/06/21",
+     Weighted_Price : "",
+    Forecast: "$60,351.60"
+  },
+  {
+    Date: "04/07/21",
+     Weighted_Price : "",
+    Forecast: "$61,097.00"
+  },
+  {
+    Date: "04/08/21",
+     Weighted_Price : "",
+    Forecast: "$60,350.91"
+  },
+  {
+    Date: "04/09/21",
+     Weighted_Price : "",
+    Forecast: "$60,924.33"
+  },
+  {
+    Date: "04/10/21",
+     Weighted_Price : "",
+    Forecast: "$61,360.38"
+  },
+  {
+    Date: "04/11/21",
+     Weighted_Price : "",
+    Forecast: "$61,883.19"
+  },
+  {
+    Date: "04/12/21",
+     Weighted_Price : "",
+    Forecast: "$62,320.73"
+  },
+  {
+    Date: "04/13/21",
+     Weighted_Price : "",
+    Forecast: "$61,813.51"
+  },
+  {
+    Date: "04/14/21",
+     Weighted_Price : "",
+    Forecast: "$62,808.03"
+  },
+  {
+    Date: "04/15/21",
+     Weighted_Price : "",
+    Forecast: "$64,609.07"
+  },
+  {
+    Date: "04/16/21",
+     Weighted_Price : "",
+    Forecast: "$65,880.65"
+  },
+  {
+    Date: "04/17/21",
+     Weighted_Price : "",
+    Forecast: "$64,953.09"
+  },
+  {
+    Date: "04/18/21",
+     Weighted_Price : "",
+    Forecast: "$62,045.41"
+  },
+  {
+    Date: "04/19/21",
+     Weighted_Price : "",
+    Forecast: "$66,587.35"
+  },
+  {
+    Date: "04/20/21",
+     Weighted_Price : "",
+    Forecast: "$66,465.25"
+  },
+  {
+    Date: "04/21/21",
+     Weighted_Price : "",
+    Forecast: "$66,339.87"
+  },
+  {
+    Date: "04/22/21",
+     Weighted_Price : "",
+    Forecast: "$67,616.09"
+  },
+  {
+    Date: "04/23/21",
+     Weighted_Price : "",
+    Forecast: "$66,403.94"
+  },
+  {
+    Date: "04/24/21",
+     Weighted_Price : "",
+    Forecast: "$66,396.83"
+  },
+  {
+    Date: "04/25/21",
+     Weighted_Price : "",
+    Forecast: "$68,163.08"
+  },
+  {
+    Date: "04/26/21",
+     Weighted_Price : "",
+    Forecast: "$68,808.61"
+  },
+  {
+    Date: "04/27/21",
+     Weighted_Price : "",
+    Forecast: "$69,622.94"
+  },
+  {
+    Date: "04/28/21",
+     Weighted_Price : "",
+    Forecast: "$68,737.27"
+  },
+  {
+    Date: "04/29/21",
+     Weighted_Price : "",
+    Forecast: "$68,288.34"
+  },
+  {
+    Date: "04/30/21",
+     Weighted_Price : "",
+    Forecast: "$69,070.93"
+  },
+  {
+    Date: ""
+  }
+]
